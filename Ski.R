dedf82584adb1a277bb5020882888b3c41a47651
dengyishuo <dengyishuou@163.com>
2025-06-05 10:57:12 +0800

    initial new hugo site

---
+++ Ski.R
@@ -0,0 +1,2 @@
+Sys.setenv(RSTUDIO_PANDOC = "/Applications/RStudio.app/Contents/MacOS")
+rmarkdown::render("/Users/matrixspk/Documents/Ski/Report001.Rmd", encoding = "UTF-8", knit_root_dir = "~/Documents/Ski")
